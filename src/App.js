c14309b2046e05f448b576728bd6e8364e9adc8f
Thomas Falcone <thomas.falcone06@gmail.com>
2019-09-16 17:49:16 +0200

    Add todo logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Wilcomen</h1>
+        <TaskList name='Todo' tasks={['Allo', 'Dont come tomorrow']} />
+        <TaskList name='WIP' tasks={['AWI', 'AWI again']} />
+      </div>
   );
+}
+
+const Task = (props) => <li>{props.text ? props.text : "No text"}</li>
+
+class TaskList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: this.props.tasks
+    };
+    this.addTask = this.addTask.bind(this);
+  }
+
+  render() {
+    return <div style={{ display: 'inline-block' }}>
+      <h2>{this.props.name}</h2>
+      <ul>
+        {this.state.tasks.map(task => <Task text={task} />)}
+      </ul>
+      <TaskForm handleClick={this.addTask} />
+    </div>
+  }
+
+  addTask(newTask) {
+    this.state.tasks.push(newTask);
+    this.setState({ tasks: this.state.tasks });
+  }
+}
+
+class TaskForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ inputValue: event.target.value })
+  }
+
+  submit() {
+    this.props.handleClick(this.state.inputValue);
+  }
+
+  render() {
+    return <div>
+      <input name='newTask' value={this.state.inputValue} onChange={this.handleChange} />
+      <input type='submit' value='Add Task' onClick={this.submit} />
+    </div>
+  }
 }
 
 export default App;
